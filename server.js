fffa03b83e5c5644e53cf830a5dbe93fbd3d9a0a
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-07-22 15:32:56 -0400

    added time out/log-out functionality with cookies

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
   secret: "Super secret secret",
-  cookie: {},
+  cookie: {
+    maxAge: 1200000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
